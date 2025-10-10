e7844f6a984d2b0421f2ca4d5d9810772382faef
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-10-10 16:55:42 -0300

    test(ToolController): add tests for findTools with a specified tag

--- src/test/java/com/apiRest/VUTTR/controllers/ToolControllerTest.java
+++ src/test/java/com/apiRest/VUTTR/controllers/ToolControllerTest.java
@@ -39,25 +39,50 @@ class ToolControllerTest {
     @Test
     @DisplayName("Should return an empty list when there's no Tool in the database")
     void findTools_findAll_Scenario01() throws Exception {
-        var response = mockMvc.perform(get("/tools"))
+        mockMvc.perform(get("/tools"))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]")
         );
     }
-
     @Test
     @DisplayName("Should return a list of all Tools available in the database")
     void findTools_findAll_Scenario02() throws Exception {
-        Tool tool1 = new Tool(null, "Notion", "https://notion.so", "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.", Arrays.asList("organization", "planning", "collaboration", "writing", "calendar"));
-        Tool tool2 = new Tool(null, "json-server", "https://github.com/typicode/json-server", "Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.", Arrays.asList("api", "json", "schema", "node", "github", "rest"));
-        Tool tool3 = new Tool(null, "fastify", "https://www.fastify.io/", "Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.", Arrays.asList("web", "framework", "node", "http2", "https", "localhost"));
-        toolRepository.saveAll(Arrays.asList(tool1, tool2, tool3));
+        createTools();
 
-        var response = mockMvc.perform(get("/tools"))
+        mockMvc.perform(get("/tools"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[*].title", containsInAnyOrder("Notion", "json-server", "fastify"))
         );
+    }
+
+    @Test
+    @DisplayName("Should return an empty list when there's no Tool in the database with the specified tag")
+    void findTools_findByTag_Scenario01() throws Exception {
+        createTools();
+
+        mockMvc.perform(get("/tools?tag=nonexistentTag"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]")
+        );
+    }
+    @Test
+    @DisplayName("Should return a list of all Tools with the specified tag available in the database")
+    void findTools_findByTag_Scenario02() throws Exception {
+        createTools();
+
+        mockMvc.perform(get("/tools?tag=node"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[*].title", containsInAnyOrder("json-server", "fastify"))
+        );
+    }
+
+    private void createTools() {
+        Tool tool1 = new Tool(null, "Notion", "https://notion.so", "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.", Arrays.asList("organization", "planning", "collaboration", "writing", "calendar"));
+        Tool tool2 = new Tool(null, "json-server", "https://github.com/typicode/json-server", "Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.", Arrays.asList("api", "json", "schema", "node", "github", "rest"));
+        Tool tool3 = new Tool(null, "fastify", "https://www.fastify.io/", "Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.", Arrays.asList("web", "framework", "node", "http2", "https", "localhost"));
+        toolRepository.saveAll(Arrays.asList(tool1, tool2, tool3));
     }
 
 }
