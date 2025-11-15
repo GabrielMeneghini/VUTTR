b11e7fa7a935ddbd6120a22abdf3ea2200c9cab6
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-11-15 11:51:06 -0300

    feat: add AuthenticationService implementing UserDetailsService

--- src/main/java/com/apiRest/VUTTR/repositories/UserRepository.java
+++ src/main/java/com/apiRest/VUTTR/repositories/UserRepository.java
@@ -4,10 +4,12 @@ import com.apiRest.VUTTR.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
-    User findByEmail(String mail);
+    Optional<User> findByEmail(String mail);
 }
---
+++ src/main/java/com/apiRest/VUTTR/services/AuthenticationService.java
@@ -0,0 +1,22 @@
+package com.apiRest.VUTTR.services;
+
+import com.apiRest.VUTTR.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email \"" + username + "\" not found."));
+    }
+
+}
