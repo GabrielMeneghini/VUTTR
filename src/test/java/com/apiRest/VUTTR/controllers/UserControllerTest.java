f697106c3a2d65a83391634af12667bfeb7bf2c2
Gabriel Meneghini <gabriel.meneghini.francelino@gmail.com>
2025-12-12 16:56:49 -0300

    test(UserControllerTest): add tests for updateAccountPassword endpoint

--- src/test/java/com/apiRest/VUTTR/controllers/UserControllerTest.java
+++ src/test/java/com/apiRest/VUTTR/controllers/UserControllerTest.java
@@ -15,14 +15,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -39,6 +39,9 @@ class UserControllerTest {
 
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setup() {
@@ -63,7 +66,7 @@ class UserControllerTest {
 
         var user = userRepository.findByEmail("emailTeste01@email.com").orElseThrow(() -> new UsernameNotFoundException("User with email \"emailTeste01@email.com\" not found."));
         assertNotNull(user);
-        assertNotEquals("12345@aB", user.getPassword());
+        assertTrue(passwordEncoder.matches("12345@aB", user.getPassword()));
         assertTrue(user.getPassword().startsWith("$2"));
     }
     @ParameterizedTest
@@ -119,7 +122,7 @@ class UserControllerTest {
     @DisplayName("Should return status 204 No Content when account is successfully deleted")
     void softDeleteAccount_Scenario01() throws Exception {
 
-        var user = userRepository.save(new User(null, "emailTeste01@email.com", "12345@aB", null));
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$Gjj45PL8nrJv0SrutUfR.uVvWt1Llz9LAb25xURO9qzZHYICkYVDy", null));
 
         var jwt = "Bearer " + tokenService.createJwt(user);
 
@@ -134,7 +137,7 @@ class UserControllerTest {
     @Test
     @DisplayName("Should return status 400 Bad Request when account is already deleted")
     void softDeleteAccount_Scenario02() throws Exception {
-        var user = userRepository.save(new User(null, "emailTeste01@email.com", "12345@aB", LocalDateTime.now()));
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$Gjj45PL8nrJv0SrutUfR.uVvWt1Llz9LAb25xURO9qzZHYICkYVDy", LocalDateTime.now()));
 
         var jwt = "Bearer " + tokenService.createJwt(user);
 
@@ -158,6 +161,96 @@ class UserControllerTest {
                         .header("Authorization", "Bearer invalid.token.here"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.message").value("JWT is invalid or expired."));
+    }
+
+    @Test
+    @DisplayName("Should return 200 Ok and update password when data is valid")
+    void updateAccountPassword_Scenario01() throws Exception {
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$pndAUm74Zy0S3X7kHxHx2eNoinSyzqkOcp4DZigAe62gouV6BW6OS", null));
+
+        var jwt = "Bearer " + tokenService.createJwt(user);
+
+        mockMvc.perform(
+                    patch("/users/account/password")
+                    .header("Authorization", jwt)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                            {
+                            	"currentPassword": "12345%aB",
+                             	"newPassword": "789&DEfg",
+                             	"confirmNewPassword": "789&DEfg"
+                            }
+                            """))
+                .andExpect(status().isOk());
+
+        var updatedPassword = userRepository.findByEmail(user.getEmail()).orElseThrow().getPassword();
+
+        assertNotEquals(user.getPassword(), updatedPassword);
+        assertTrue(passwordEncoder.matches("12345%aB", user.getPassword()));
+        assertTrue(passwordEncoder.matches("789&DEfg", updatedPassword));
+    }
+    @Test
+    @DisplayName("Should return 400 Bad Request when current password is incorrect")
+    void updateAccountPassword_Scenario02() throws Exception {
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$pndAUm74Zy0S3X7kHxHx2eNoinSyzqkOcp4DZigAe62gouV6BW6OS", null));
+
+        var jwt = "Bearer " + tokenService.createJwt(user);
+
+        mockMvc.perform(
+                        patch("/users/account/password")
+                                .header("Authorization", jwt)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                            {
+                            	"currentPassword": "11111111",
+                             	"newPassword": "789&DEfg",
+                             	"confirmNewPassword": "789&DEfg"
+                            }
+                            """))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Current password is incorrect."));
+    }
+    @Test
+    @DisplayName("Should return 400 Bad Request when PASSWORDS do not match")
+    void updateAccountPassword_Scenario03() throws Exception {
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$pndAUm74Zy0S3X7kHxHx2eNoinSyzqkOcp4DZigAe62gouV6BW6OS", null));
+
+        var jwt = "Bearer " + tokenService.createJwt(user);
+
+        mockMvc.perform(
+                        patch("/users/account/password")
+                                .header("Authorization", jwt)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                            {
+                            	"currentPassword": "12345%aB",
+                             	"newPassword": "789&DEfg",
+                             	"confirmNewPassword": "11111111"
+                            }
+                            """))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message[0]").value("Passwords do not match"));
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"12345@ab", "12345aaB", "abcde@aB", "12345@AB", "2345@aB", " "})
+    @DisplayName("Should return status 400 Bad Request when PASSWORD is not valid")
+    void updateAccountPassword_Scenario04(String password) throws Exception {
+        var user = userRepository.save(new User(null, "emailTeste01@email.com", "$2a$12$k1PFNZH6JJcB/z4GboQWoOhad5e56fKcZT6JlBlUHqZrkRf3ZnCtC", null));
+
+        var jwt = "Bearer " + tokenService.createJwt(user);
+
+        mockMvc.perform(
+                        patch("/users/account/password")
+                                .header("Authorization", jwt)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "currentPassword": "12345@aB",
+                                            "newPassword": "%s",
+                                            "confirmNewPassword": "%s"
+                                        }
+                                """.formatted(password, password)))
+                .andExpect(status().isBadRequest());
     }
 
 }
